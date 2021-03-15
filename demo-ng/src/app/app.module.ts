2c9be5c968ab345c5f6868afd191a230e9fb035e
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-03-15 00:51:42 +0000

    Release 2.0.1: Fixes for dependency error

--- demo-ng/src/app/app.module.ts
+++ demo-ng/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule, NativeScriptRouterModule } from "@nativescript/angular";
+import { HideActionBarModule } from '@microexcel-csd/nativescript-hide-action-bar/angular';
 import { AppComponent } from "./app.component";
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +11,8 @@ import { AppRoutingModule } from './app-routing.module';
     imports: [
         NativeScriptModule,
         NativeScriptRouterModule,
-        AppRoutingModule
+        AppRoutingModule,
+        HideActionBarModule
     ],
     declarations: [
         AppComponent,
